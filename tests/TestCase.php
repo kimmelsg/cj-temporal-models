9079ddff49b74680a468abf26ed4dd1ede418761
Josh Forbes <jforbes@kimmel.com>
2016-10-23 19:50:29 -0400

    Base test case.

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NavJobs\Temporal\Test;
+
+use PHPUnit_Framework_TestCase;
+
+abstract class TestCase extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+    }
+}
